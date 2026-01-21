36b633511d3184ab379d9ae7254f09274cfeb860
Voutsa Stevie <machevoutsa@gmail.com>
2026-01-21 16:50:41 +0100

    ref: implementing 'bannir utilisateur'

--- src/app/features/admin/admin.ts
+++ src/app/features/admin/admin.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, effect, signal } from '@angular/core';
 import { BadgeModule } from 'primeng/badge';
 import { TableModule } from 'primeng/table';
 import { UserService } from '../../core/users/services/user.service';
@@ -14,14 +14,19 @@ import { User } from '../../core/users/interfaces/user';
 export class AdminComponent {
   private userService = inject(UserService);
 
-  users = this.userService.users;
+  displayedUsers = signal<User[]>([]);
 
   constructor() {
     this.userService.list();
+
+    effect(() => {
+      this.displayedUsers.set(this.userService.users());
+    });
   }
 
-  ban(user: User) {
-    console.log('Bannir :', user);
+  ban(user: User): void {
+    const confirmed = window.confirm(`Voulez-vous vraiment bannir ${user.username} ?`);
+    if (!confirmed) return;
+    this.displayedUsers.update(users => users.filter(u => u.id !== user.id));
   }
-
 }
